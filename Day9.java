1c2df940084415d6f2c72edb6f7c8060e8ddb876
Gabzuno <kgperez120@gmail.com>
2025-07-31 23:44:37 +0800

    Day9

--- Day9.java
+++ Day9.java
@@ -1,3 +1,11 @@
-public class Day9 {
+import java.util.Scanner;
 
+public class Day9 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+
+        } catch (Exception e) {
+            System.out.println("Something went Wrong. Try again.");
+        }
+    }
 }
