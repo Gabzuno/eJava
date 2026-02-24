d302afa29503a1e884ebc5d163d608a6b2772ecc
gabzuno <kgperez120@gmail.com>
2026-02-24 08:25:45 +0800

    adding Runtime and Compiletime Polymorpism

--- OOP/Main.java
+++ OOP/Main.java
@@ -5,13 +5,18 @@ public class Main {
         inventory Inventory = new inventory();
 
         item newItem = new item("Generic Item",10);
-        fruit newFruit = new fruit("Apple", 10, "Fuji");
-        weapon newWeapon = new weapon("Sword", 1, 100, "Melee");
+        // fruit newFruit = new fruit("Apple", 10, "Fuji");
+        // weapon newWeapon = new weapon("Sword", 1, 100, "Melee");
 
         Inventory.addItem(newItem);
-        Inventory.addItem(newFruit);
-        Inventory.addItem(newWeapon);
+        Inventory.addItem("Apple", 10, "Fuji");
+        Inventory.addItem("Sword", 1, 100, "Melee");
+        Inventory.addItem("Bow", 1, 80, "Ranged");
 
         Inventory.displayInventory();
+
+        System.out.println("\nFiltered Inventory (Type: Ranged):");
+        Inventory.displayInventory("Ranged");
+
     }
 }
--- OOP/inventory.java
+++ OOP/inventory.java
@@ -14,10 +14,31 @@ public class inventory {
         items.add(newItem);
     }
 
+    // Method Overloading - Compile Time Polymorphism
+    public void addItem(String name, int quantity, String type) {
+        items.add(new fruit(name, quantity, type));
+    }
+
+    public void addItem(String name, int quantity, int damage, String type) {
+        items.add(new weapon(name, quantity, damage, type));
+    }
+
     // Dynaic Polymorphism - Runtime Polymorphism
     public void displayInventory(){
         for(item item : items){
             System.out.println(item.toString());
+        }
+    }
+
+    // Overloading - Compile Time Polymorphism
+    public void displayInventory(String filterType){
+        for(item item : items){
+            if(item instanceof fruit && ((fruit) item).getType().equalsIgnoreCase(filterType)){
+                System.out.println(item.toString());
+            }
+            else if(item instanceof weapon && ((weapon) item).getType().equalsIgnoreCase(filterType)){
+                System.out.println(item.toString());
+            }
         }
     }
 }
